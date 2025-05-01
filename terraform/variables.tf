5373f298153115f8a3138abd262729334e4b0081
shuo.liu <shuo.liu@flybuys.com.au>
2025-05-01 21:20:15 +1000

    feat: add terraform

---
+++ terraform/variables.tf
@@ -0,0 +1,9 @@
+variable "domain_name" {
+  description = "The domain name for the website"
+  type        = string
+}
+
+variable "subdomain" {
+  description = "The domain name for the website"
+  type        = string
+}
